303fdc5d93263cf84475eb5a588f420b47c769f4
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-12 18:10:46 +0800

    added test component

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,23 @@
+<template>
+<h1>
+  This is a test component!
+</h1>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="scss" scoped>
+h1{
+  padding: 1rem;
+  border: 1px solid #444;
+  display: inline;
+  border-radius: 1rem;
+}
+h1:hover{
+  background: black;
+  color: white;
+  border-color: black;
+}
+</style>
--- src/views/Test.vue
+++ src/views/Test.vue
@@ -1,11 +1,13 @@
 <template>
 <div>
   <h1>Hello world!</h1>
+  <TestComponent/>
 </div>
 </template>
 
 <script lang="ts" setup>
 
+import TestComponent from "@/components/TestComponent.vue";
 </script>
 
 <style lang="scss">
