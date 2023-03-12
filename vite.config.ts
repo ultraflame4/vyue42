afd6a29032b18dfcc0e222fde1444102e339ba35
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-12 18:27:17 +0800

    added dts generation to build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,38 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import {fileURLToPath, URL} from 'node:url'
+import dts from 'vite-plugin-dts'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [
+        vue(),
+        dts({
+            include: ['src/components',"src/index.ts"],
+            skipDiagnostics: false,
+        })
+    ],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src', import.meta.url))
+        }
+    },
+    build: {
+        lib: {
+            entry: path.resolve(__dirname, "src/index.ts"),
+            name: "vyue42"
+        },
+        outDir: "./lib",
+        cssCodeSplit: true,
+        rollupOptions: {
+            external: ["vue", "vue-router"],
+            output: {
+                globals: {
+                    vue: "Vue"
+                }
+            }
+        }
     }
-  }
+
 })
