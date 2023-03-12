68eaac8de1259a0ab4d65aebbca155e334a3085c
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-03-12 21:06:43 +0800

    we got ourselfs a sticky directive

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,43 @@
+import type {App} from "vue";
+import toPx from "to-px";
+
 export {default as TestComponent} from "./components/TestComponent.vue"
+
+const plugin ={
+    install: (app:App)=>{
+        app.directive('stuck',(el:HTMLElement, binding) => {
+            const style = window.getComputedStyle(el)
+
+            function formatVal(n: string) {
+                let val = toPx(n)
+                if (val)
+                    return `-${val+1}px`
+                return "100%"
+            }
+
+            const top = formatVal(style.top)
+            const bottom = formatVal(style.bottom)
+            const left = formatVal(style.left)
+            const right = formatVal(style.right)
+            console.log(`${top} ${right} ${bottom} ${left}`)
+            const observer = new IntersectionObserver(entries => {
+                console.log(entries[0])
+                if (!entries[0].isIntersecting){
+                    console.log("Stickied!",entries[0].intersectionRatio)
+                    el.setAttribute("stuck","")
+                }
+                else {
+                    el.removeAttribute("stuck")
+                }
+            },{
+                threshold:[1],
+                rootMargin: `${top} ${right} ${bottom} ${left}`
+            })
+
+            observer.observe(el)
+
+        })
+    }
+}
+
+export default plugin
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,13 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/index.css'
+import plugin from "@/index";
 
 const app = createApp(App)
 
 app.use(router)
+app.use(plugin)
+
 
 app.mount('#app')
+
