474dcb97a753681ec19383ed3df5e146973bb298
ultraflame42 <34125174+ultraflame4@users.noreply.github.com>
2023-04-01 11:25:37 +0800

    fixed import problems with iRatio

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import {vstuck} from "@/directives/vstuck";
 import {iratio} from "@/directives/iratio";
 
 export {default as TestComponent} from "./components/TestComponent.vue"
+export {iRatioObject,type IRatioConfig} from "@/directives/iratio"
 
 export const vyue42 ={
     install: (app:App)=>{
--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
         }
     },
     build: {
+
         lib: {
             entry: path.resolve(__dirname, "src/index.ts"),
             formats: ['es', 'umd'],
